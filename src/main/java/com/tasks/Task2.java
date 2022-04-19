f27336092c6b2d3e73da52dbe094800f113fddb0
admin <https://github.com/MaksimPivo/Java.git>
2022-04-19 20:55:36 +0300

    Task 2 update

--- src/main/java/com/tasks/Task2.java
+++ src/main/java/com/tasks/Task2.java
@@ -1,12 +1,14 @@
 package com.tasks;
 
 import com.Utis.Gruppyi;
+import com.Utis.Studentyi;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class Task2 {
     private final Session session;
@@ -19,10 +21,16 @@ public class Task2 {
         Transaction t1 = session.beginTransaction();
         int year = LocalDate.now().getYear();
         List<Gruppyi> q = session.createQuery("from Gruppyi g").list();
+        List<Studentyi> q2 = session.createQuery("from Studentyi s").list();
 
         for (Gruppyi g : q) {
             if (year - Integer.parseInt(g.getDataFormir().toString().split("-")[0]) >= 9) {
                 g.setStatus("rasformirovana");
+                for (Studentyi s : q2) {
+                    if (s.getGruppyi().getShifr().equals(g.getShifr())) {
+                        s.setStatus("vyipusknik");
+                    }
+                }
             }
             g.setStatusDate(new Date());
             session.update(g);
