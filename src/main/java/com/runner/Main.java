692d2832e859aa243ba316d7b661fa2abed510ac
admin <https://github.com/MaksimPivo/Java.git>
2022-04-19 20:47:23 +0300

    Task 2

--- src/main/java/com/runner/Main.java
+++ src/main/java/com/runner/Main.java
@@ -1,18 +1,8 @@
 package com.runner;
 
-import com.Utis.Gruppyi;
 import com.Utis.NewHibernateUtil;
-import com.Utis.Studentyi;
-import com.tasks.Task1;
 import com.tasks.Task2;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
