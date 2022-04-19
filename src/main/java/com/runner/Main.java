440995e110bd03e037510f1b0aa244e8777f10d7
admin <https://github.com/MaksimPivo/Java.git>
2022-04-19 22:26:59 +0300

    Task 3 create table spetsialnosti

--- src/main/java/com/runner/Main.java
+++ src/main/java/com/runner/Main.java
@@ -1,15 +1,29 @@
 package com.runner;
 
 import com.Utis.NewHibernateUtil;
-import com.tasks.Task2;
+import com.Utis.Spetsialnosti;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Session s = NewHibernateUtil.getSessionFactory().openSession();
 
-        Task2 task2 = new Task2(s);
-        task2.updateGruppyi();
+        Date date = new Date();
+//        date.setYear(2013); //неправилный парсинг даты(3913)
+        date.setYear(113);
+        date.setMonth(3);
+        date.setDate(23);
+        Spetsialnosti spetsialnosti = new Spetsialnosti();
+        Transaction t = s.beginTransaction();
+        spetsialnosti.setKodSpets(2);
+        spetsialnosti.setDateFormir(date);
+        spetsialnosti.setNazvanie("Programmnoe obespechenie informationnih sistem");
+        spetsialnosti.setStatusDate(new Date());
+        t.commit();
+        s.save(spetsialnosti);
 
         s.close();
     }
